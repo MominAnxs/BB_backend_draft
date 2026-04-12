39f08dcfc21fdd837fc1319f1044b8008517e305
MominAnxs <mominanas576@gmail.com>
2026-04-12 22:43:04 +0530

    PageClient added

--- app/adminland/incidents/page.tsx
+++ app/adminland/incidents/page.tsx
@@ -1,5 +1,10 @@
-"use client";
-import { IncidentData } from '@/adminland/IncidentData';
-export default function IncidentsPage() {
-  return <IncidentData />;
+import { Suspense } from 'react';
+import PageClient from './PageClient';
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PageClient />
+    </Suspense>
+  );
 }
