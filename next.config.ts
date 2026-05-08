12a81b68fe2122367470619cc1f797ed8f07bea2
MominAnxs <mominanas576@gmail.com>
2026-05-09 00:02:08 +0530

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  dev: {
-    turbopack: {
-      enabled: false,
-    },
-  },
 };
 
 export default nextConfig;
